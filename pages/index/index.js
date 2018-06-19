0de4b9d556c402b02bb5217f72368047314b5034
oneminuter <1050086935@qq.com>
2018-06-20 01:34:42 +0800

    首页样式100%

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,24 @@
 // pages/index/index.js
+import {
+  con,
+  util
+} from '../../utils/factory.js'
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    img: con.IMGS
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
--- utils/const.js
+++ utils/const.js
@@ -1,3 +1,12 @@
 export class Const {
-  USERINFOSTORAGE = 'userinfo'
+  USERINFOSTORAGE = 'userinfo';
+  IMGS = {
+    indexCardBg: "http://oneminuter.com/images/gbyk/bg.png",
+    iconAvator: "http://oneminuter.com/images/gbyk/avator.png",
+    iconCollect: "http://oneminuter.com/images/gbyk/collect.png",
+    iconDetail: "http://oneminuter.com/images/gbyk/detail.png",
+    iconSearch: "http://oneminuter.com/images/gbyk/search.png",
+    iconShare: "http://oneminuter.com/images/gbyk/share.png",
+    iconThumbUp: "http://oneminuter.com/images/gbyk/thumb_up.png",
+  }
 }
